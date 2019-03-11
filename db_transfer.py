1ff5439ad2276f5e1d67279709b701db3f18272b
balala <xiongxin2oo8@vip.qq.com>
2019-03-11 12:54:45 +0800

    bug fix

--- db_transfer.py
+++ db_transfer.py
@@ -453,7 +453,7 @@ class Dbv3Transfer(DbTransfer):
 		conn.autocommit(True)
 
 		save_flow=''
-
+        update_ssr_user='';
 		for id in dt_transfer.keys():
 			transfer = dt_transfer[id]
 			bandwidth_thistime = bandwidth_thistime + transfer[0] + transfer[1]
@@ -466,6 +466,7 @@ class Dbv3Transfer(DbTransfer):
 				del self.user_pass[id]
 				
 			save_flow += 'INSERT INTO `saveFlow`(`id`, `accountId`, `port`, `flow`, `time`) SELECT %s,accountId,%s,%s,%s FROM `account_flow` WHERE port=%s AND serverId=%s;' %(str(self.cfg["node_id"]),id,int(transfer[0])+int(transfer[1]),str(int(last_time))+'000',id,str(self.cfg["node_id"]))
+			update_ssr_user+='UPDATE ssr_user set t=%s,u=u+%s,d=d+%s where port=%s and serverId=%s;' % (str(int(last_time)),int(transfer[0]),int(transfer[1],id,str(self.cfg["node_id"]))
 			query_sub_when += ' WHEN %s THEN u+%s' % (id, int(transfer[0] * self.cfg["transfer_mul"]))
 			query_sub_when2 += ' WHEN %s THEN d+%s' % (id, int(transfer[1] * self.cfg["transfer_mul"]))
 			update_transfer[id] = transfer
@@ -502,7 +503,8 @@ class Dbv3Transfer(DbTransfer):
 						' WHERE port IN (%s)' % query_sub_in
 			cur = conn.cursor()
 			try:
-				cur.execute(query_sql)
+				#cur.execute(query_sql)
+				cur.execute(update_ssr_user)
 			except Exception as e:
 				logging.error(e)
 			cur.close()
