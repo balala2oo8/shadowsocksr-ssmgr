b3cacee5a3d92121b9601e71336113abfd97159b
balala <xiongxin2oo8@vip.qq.com>
2019-03-11 13:09:31 +0800

    bug fix

--- db_transfer.py
+++ db_transfer.py
@@ -453,7 +453,7 @@ class Dbv3Transfer(DbTransfer):
 		conn.autocommit(True)
 
 		save_flow=''
-        update_ssr_user=''
+		update_ssr_user=''
 		
 		for id in dt_transfer.keys():
 			transfer = dt_transfer[id]
