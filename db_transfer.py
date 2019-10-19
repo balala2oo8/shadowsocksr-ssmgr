c3563a501ecd72083fefcda17d7ae3180af8b8c0
balala <xiongxin2oo8@vip.qq.com>
2019-10-19 20:02:50 +0800

    update

--- db_transfer.py
+++ db_transfer.py
@@ -514,8 +514,8 @@ class Dbv3Transfer(DbTransfer):
 		#记录在线ip
 		alive_sql='"INSERT INTO `alive_ip` (`id`, `nodeid`,`userid`, `ip`, `datetime`) VALUES (NULL,0,0,0,100000)'	
 		online_iplist = ServerPool.get_instance().get_servers_iplist()
-        for id in online_iplist.keys():
-            for ip in online_iplist[id]:
+		for id in online_iplist.keys():
+			for ip in online_iplist[id]:
 				alive_sql = alive_sql +",(NULL, '" + str(self.cfg["node_id"]) + "','" + str(self.cfg["node_id"]) + "', '" + str(ip) + "', unix_timestamp())"
 		cur = conn.cursor()
 		try:
